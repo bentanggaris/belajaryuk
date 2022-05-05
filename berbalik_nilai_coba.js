6d8f1c91537b0ff4bd069cb726e453fd4837db97
unknown <kopipribadiku@gmail.com>
2022-05-06 01:41:02 +0800

    commit pertama

---
+++ berbalik_nilai_coba.js
@@ -0,0 +1,149 @@
+$(document).ready(function(){
+
+	function soalbagian1() {
+		let url = "berbalik_nilai_coba.json";
+		$.getJSON(url, function(data){
+			let random = Math.floor(Math.random()*data.berbalik_nilai1.length);
+			$("#randomsoal1").html(data.berbalik_nilai1[random].soal1);
+			
+			$("#inputjawaban1").html(data.berbalik_nilai1[random].jawab1);
+		})
+	}
+
+	soalbagian1();
+
+	function jwbsoal1(){		
+		let a = parseInt($("#jawaban1").val());
+		let b = parseInt($("#inputjawaban1").html());
+		
+		if(a===b){
+			$("#output1").html("<img src='img/benar.svg' style= 'width: 30px;'>");
+		}
+		else{
+			$("#output1").html("<img src='img/salah.svg' style= 'width: 30px;'>");
+		}
+	}
+	
+	function soalbagian2() {
+		let url = "berbalik_nilai_coba.json";
+		$.getJSON(url, function(data){
+			let random = Math.floor(Math.random()*data.berbalik_nilai2.length);
+			$("#randomsoal2").html(data.berbalik_nilai2[random].soal2);
+			
+			$("#inputjawaban2").html(data.berbalik_nilai2[random].jawab2);
+		})
+	}
+
+	soalbagian2();
+	function jwbsoal2(){		
+		let a = parseInt($("#jawaban2").val());
+		let b = parseInt($("#inputjawaban2").html());
+		
+		if(a===b){
+			$("#output2").html("<img src='img/benar.svg' style= 'width: 30px;'>");
+		}
+		else{
+			$("#output2").html("<img src='img/salah.svg' style= 'width: 30px;'>");
+		}
+	}
+	
+	function soalbagian3() {
+		let url = "berbalik_nilai_coba.json";
+		$.getJSON(url, function(data){
+			let random = Math.floor(Math.random()*data.berbalik_nilai3.length);
+			$("#randomsoal3").html(data.berbalik_nilai3[random].soal3);
+			
+			$("#inputjawaban3").html(data.berbalik_nilai3[random].jawab3);
+		})
+	}
+
+	soalbagian3();
+	function jwbsoal3(){		
+		let a = parseInt($("#jawaban3").val());
+		let b = parseInt($("#inputjawaban3").html());
+		
+		if(a===b){
+			$("#output3").html("<img src='img/benar.svg' style= 'width: 30px;'>");
+		}
+		else{
+			$("#output3").html("<img src='img/salah.svg' style= 'width: 30px;'>");
+		}
+	}
+
+	function soalbagian4() {
+		let url = "berbalik_nilai_coba.json";
+		$.getJSON(url, function(data){
+			let random = Math.floor(Math.random()*data.berbalik_nilai4.length);
+			$("#randomsoal4").html(data.berbalik_nilai4[random].soal4);
+			
+			$("#inputjawaban4").html(data.berbalik_nilai4[random].jawab4);
+		})
+	}
+
+	soalbagian4();
+	function jwbsoal4(){		
+		let a = parseInt($("#jawaban4").val());
+		let b = parseInt($("#inputjawaban4").html());
+		
+		if(a===b){
+			$("#output4").html("<img src='img/benar.svg' style= 'width: 30px;'>");
+		}
+		else{
+			$("#output4").html("<img src='img/salah.svg' style= 'width: 30px;'>");
+		}
+	}
+
+
+	function soalbagian5() {
+		let url = "berbalik_nilai_coba.json";
+		$.getJSON(url, function(data){
+			let random = Math.floor(Math.random()*data.berbalik_nilai5.length);
+			$("#randomsoal5").html(data.berbalik_nilai5[random].soal5);
+			
+			$("#inputjawaban5").html(data.berbalik_nilai5[random].jawab5);
+		})
+	}
+
+	soalbagian5();
+	function jwbsoal5(){		
+		let a = parseInt($("#jawaban5").val());
+		let b = parseInt($("#inputjawaban5").html());
+		
+		if(a===b){
+			$("#output5").html("<img src='img/benar.svg' style= 'width: 30px;'>");
+		}
+		else{
+			$("#output5").html("<img src='img/salah.svg' style= 'width: 30px;'>");
+		}
+	}
+	
+	$("#cekjawaban").on("click", function(){
+		jwbsoal1();
+		jwbsoal2();
+		jwbsoal3();
+		jwbsoal4();
+		jwbsoal5();				
+	})
+	
+	$("#cobalagi").on("click", function(){
+		soalbagian1();
+		soalbagian2();
+		soalbagian3();
+		soalbagian4();
+		soalbagian5();		
+
+		$("#jawaban1").val("");
+		$("#jawaban2").val("");
+		$("#jawaban3").val("");
+		$("#jawaban4").val("");		
+		$("#jawaban5").val("");
+
+		$("#output1").html("");
+		$("#output2").html("");
+		$("#output3").html("");
+		$("#output4").html("");
+		$("#output5").html("");
+
+
+	})
+})
--- soal_evaluasi_fix.js
+++ soal_evaluasi_fix.js
@@ -1,9 +1,9 @@
 // Merandom soal
 let randomEvaluasi=evaluasi.sort((a, b)=>{return 0.5 - Math.random()})
 
-generate(0);
+soalSoal(0);
 
-function generate(index) {
+function soalSoal(index) {
     document.getElementById("soaltampil").innerHTML = evaluasi[index].soal;
     document.getElementById("pil1").innerHTML = evaluasi[index].a;
     document.getElementById("pil2").innerHTML = evaluasi[index].b;
@@ -11,30 +11,30 @@ function generate(index) {
     document.getElementById("pil4").innerHTML = evaluasi[index].d;    
 }
 
-let i = 0;
-let jwbBenar
+// let i = 0;
+// let jwbBenar
 
-function cekBenarnya() {
+// function cekBenarnya() {
 
-    if (document.getElementById("a").checked && evaluasi[i].a == evaluasi[i].jawaban) {
-       jwbBenar++;
-    }
-    if (document.getElementById("b").checked && evaluasi[i].b == evaluasi[i].jawaban) {
-        jwbBenar++;
-    }
-    if (document.getElementById("c").checked && evaluasi[i].c == evaluasi[i].jawaban) {
-        jwbBenar++;
-    }
-    if (document.getElementById("d").checked && evaluasi[i].d == evaluasi[i].jawaban) {
-        jwbBenar++;
-    }
-}
+//     if (document.getElementById("a").checked && evaluasi[i].a == evaluasi[i].jawaban) {
+//        jwbBenar++;
+//     }
+//     if (document.getElementById("b").checked && evaluasi[i].b == evaluasi[i].jawaban) {
+//         jwbBenar++;
+//     }
+//     if (document.getElementById("c").checked && evaluasi[i].c == evaluasi[i].jawaban) {
+//         jwbBenar++;
+//     }
+//     if (document.getElementById("d").checked && evaluasi[i].d == evaluasi[i].jawaban) {
+//         jwbBenar++;
+//     }
+// }
 
-let totalBenarnya = 20 - jwbBenar
-let totalNilai = totalBenarnya * 10
+// let totalBenarnya = 20 - jwbBenar
+// let totalNilai = totalBenarnya * 10
 
-function cekHasil() {
-    document.write("<body style='background-color:#FCF4EC;'>");
-    document.write("<div style='margin: 0 auto; padding: 3%; width: 500px; background-color: none; color:#282828; font-size:1rem; text-align:center; border: 3px #282828 solid;  border-radius:10px;'>Total Nilaimu : "+totalNilai+"</div>");
-    document.write("</body>");
-}
+// function cekHasil() {
+//     document.write("<body style='background-color:#FCF4EC;'>");
+//     document.write("<div style='margin: 0 auto; padding: 3%; width: 500px; background-color: none; color:#282828; font-size:1rem; text-align:center; border: 3px #282828 solid;  border-radius:10px;'>Total Nilaimu : "+totalNilai+"</div>");
+//     document.write("</body>");
+// }
